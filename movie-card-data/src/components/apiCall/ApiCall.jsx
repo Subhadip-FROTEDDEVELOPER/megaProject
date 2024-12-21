811b7a380027f8a237480974cbfe879e3da7d50f
Subhadip Pradhan <pradhansubhadip1999@gmail.com>
2024-12-21 22:21:54 +0530

    update ApiCall component to fetch Pikachu data and enhance rendering

--- movie-card-data/src/components/apiCall/ApiCall.jsx
+++ movie-card-data/src/components/apiCall/ApiCall.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 
 const ApiCall = () => {
-  const [pokemon, setPokemon] = useState({});
-  const API = "https://pokeapi.co/api/v2/pokemon/1";
+  const [pokemon, setPokemon] = useState([]);
+  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
   useEffect(() => {
     fetch(API)
       .then((res) => res.json())
@@ -11,17 +11,36 @@ const ApiCall = () => {
       })
       .catch(() => console.log(error));
   }, []);
+  console.log(pokemon);
   return (
-    <>
-      {pokemon.name && (
-        <div key={pokemon.id}>
+    <section className="container">
+      <header>
+        <h1> Lets Catch Pokémon</h1>
+      </header>
+      <ul className="card-demo">
+        <li className="pokemon-card">
+          <figure>
+            <img
+              src={pokemon.sprites?.front_default}
+              alt={pokemon.name}
+              className="pokemon-image"
+            />
+          </figure>
           <h1>{pokemon.name}</h1>
-          {pokemon.sprites && (
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-          )}
-        </div>
-      )}
-    </>
+          <div className="grid-three-cols">
+            <p className="pokemon-info">
+              Height: <span> {pokemon.height} </span>
+            </p>
+            <p className="pokemon-info">
+              Weight: <span> {pokemon.weight}</span>
+            </p>
+            <p className="pokemon-info">
+              {/* speed: <span>{pokemon.stats[5].base_stat}</span> */}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </section>
   );
 };
 
