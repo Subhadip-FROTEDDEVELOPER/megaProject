25914868b9d04dc2f7dc2c207e981328c3117b42
Subhadip Pradhan <pradhansubhadip1999@gmail.com>
2024-12-21 22:33:28 +0530

    refactor ApiCall component to handle null state and improve rendering logic

--- movie-card-data/src/components/apiCall/ApiCall.jsx
+++ movie-card-data/src/components/apiCall/ApiCall.jsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useState } from "react";
 
 const ApiCall = () => {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(null);
   const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
+
   useEffect(() => {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setPokemon(data);
       })
-      .catch(() => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
+
   console.log(pokemon);
   return (
     <section className="container">
       <header>
-        <h1> Lets Catch Pokémon</h1>
+        <h1> Let's Catch Pokémon</h1>
       </header>
       <ul className="card-demo">
-        <li className="pokemon-card">
-          <figure>
-            <img
-              src={pokemon.sprites?.front_default}
-              alt={pokemon.name}
-              className="pokemon-image"
-            />
-          </figure>
-          <h1>{pokemon.name}</h1>
-          <div className="grid-three-cols">
-            <p className="pokemon-info">
-              Height: <span> {pokemon.height} </span>
-            </p>
-            <p className="pokemon-info">
-              Weight: <span> {pokemon.weight}</span>
-            </p>
-            <p className="pokemon-info">
-              {/* speed: <span>{pokemon.stats[5].base_stat}</span> */}
-            </p>
-          </div>
-        </li>
+        {pokemon && (
+          <li className="pokemon-card">
+            <figure>
+              <img
+                src={pokemon.sprites?.front_default}
+                alt={pokemon.name}
+                className="pokemon-image"
+              />
+            </figure>
+            <h1>{pokemon.name}</h1>
+            <div className="grid-three-cols">
+              <p className="pokemon-info">
+                Height: <span> {pokemon.height} </span>
+              </p>
+              <p className="pokemon-info">
+                Weight: <span> {pokemon.weight}</span>
+              </p>
+              <p className="pokemon-info">
+                Speed: <span>{pokemon.stats[5].base_stat}</span>
+              </p>
+            </div>
+          </li>
+        )}
       </ul>
     </section>
   );
